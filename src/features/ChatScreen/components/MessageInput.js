c856b31fd0874ae2f618733c01fad821fa253d71
Sean Mizen <sean@seanmizen.com>
2022-03-22 20:09:03 +0000

    expanded messageInput

--- src/features/ChatScreen/components/MessageInput.js
+++ src/features/ChatScreen/components/MessageInput.js
@@ -1,13 +1,34 @@
-import { useRef } from "react";
-const MessageInput = ({ chat }) => {
+import styles from "../ChatScreen.module.css";
+import { useRef, useState } from "react";
+
+const MessageInput = ({ chat, submitMessageBody }) => {
   const inputRef = useRef();
+  const submitRef = useRef();
+  const [messageBody, setMessageBody] = useState();
+
   const onSubmit = (e) => {
-    e.preventDefault();
+    // e.preventDefault();
+  };
+
+  const keyDown = (e) => {
+    if (e.keyCode === 13 && e.shiftKey === false) {
+      e.preventDefault();
+      console.log("Submitting " + messageBody + "!");
+      submitRef.current.click();
+    }
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      <input ref={inputRef} />
+    <form className={styles["message-input-form"]} onSubmit={onSubmit}>
+      <textarea
+        onKeyDown={(e) => keyDown(e)}
+        onChange={(e) => setMessageBody(e.target.value)}
+        className={styles["message-input"]}
+        ref={inputRef}
+      />
+      <button type="submit" ref={submitRef}>
+        &gt;
+      </button>
     </form>
   );
 };
