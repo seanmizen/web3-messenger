492ede4aef0b9acc924a707c2ee7b3dc96784492
Sean Mizen <sean@seanmizen.com>
2022-03-24 11:36:06 +0000

    introduced receipt info

--- src/features/ChatScreen/components/Message.js
+++ src/features/ChatScreen/components/Message.js
@@ -1,6 +1,30 @@
+import { useEffect, useState } from "react";
 import styles from "../ChatScreen.module.css";
+
+const receiptWatcher = styles["receipt-watcher"];
+const noReceipt = styles["no-receipt"];
+const hasReceipt = styles["has-receipt"];
+const transactionSuccess = styles["transaction-success"];
+const transactionFailure = styles["transaction-failure"];
+
 const Message = ({ message, isCurrentUser, isSequential }) => {
+  const [receiptStyle, setReceiptStyle] = useState(noReceipt);
   const timestamp = new Date(parseInt(message.unixTimeStamp));
+
+  // useEffect for monitoring message status
+  useEffect(() => {
+    console.log("message receipt status changed");
+    if (true) {
+      setReceiptStyle(noReceipt);
+    } else if (true) {
+      setReceiptStyle(hasReceipt);
+    } else if (true) {
+      setReceiptStyle(transactionSuccess);
+    } else if (true) {
+      setReceiptStyle(transactionFailure);
+    }
+  }, [message.receiptStatus]);
+
   return (
     <li
       className={
@@ -21,6 +45,14 @@ const Message = ({ message, isCurrentUser, isSequential }) => {
         </div>
         <div className={styles["timestamp"]}>
           {timestamp.toLocaleTimeString()}
+        </div>
+        <div className={styles["receipt-overlay"]}>
+          <div className={receiptWatcher + " " + receiptStyle}>
+            <div className={styles["receipt-info"]}>
+              {JSON.stringify(message.receiptStatus) ||
+                "Receipt information not available."}
+            </div>
+          </div>
         </div>
       </div>
     </li>
